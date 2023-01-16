35b05ba9483de4237642f85a81535e679f22b95f
Roman <roman@roman-pop-os.fritz.box>
2023-01-16 14:13:40 +0100

    delays feature added

--- src/rlflo/rlflo.h
+++ src/rlflo/rlflo.h
@@ -28,6 +28,9 @@
 
 #include "map/scl/sclSize.h"
 #include "base/main/mainInt.h"
+#include "base/abc/abc.h"
+#include "opt/rwr/rwr.h"
+#include "bool/dec/dec.h"
 
 ////////////////////////////////////////////////////////////////////////
 ///                         PARAMETERS                               ///
@@ -47,7 +50,7 @@ ABC_NAMESPACE_HEADER_START
 ///                    FUNCTION DECLARATIONS                         ///
 ////////////////////////////////////////////////////////////////////////
 
-extern void Abc_RLfLOMapGetAreaDelay( Abc_Frame_t * pAbc, float * pArea, float * pDelay, int fAreaOnly, int useDelayTarget, double DelayTargetArg, int nTreeCRatio, int fUseWireLoads );
+extern void Abc_RLfLOMapGetAreaDelay( Abc_Frame_t * pAbc, float * pArea, float * pDelay, int fAreaOnly, int useDelayTarget, double DelayTargetArg, int nTreeCRatio, int fUseWireLoads, int getDelays, double * pDelays );
 extern void Abc_RLfLOGetNodeFeatures( Abc_Frame_t * pAbc, float * x, size_t n, size_t p);
 extern void Abc_RLfLOGetMaxDelayTotalArea( Abc_Frame_t * pAbc, float * pMaxDelay, float * pTotalArea, int nTreeCRatio, int fUseWireLoads, int fShowAll, int fPrintPath, int fDumpStats );
 extern void Abc_RLfLOGetNumNodesAndLevels( Abc_Frame_t * pAbc, int * pNumNodes, int * pNumLevels );
@@ -57,7 +60,9 @@ extern void Abc_RLfLOGetNumEdges( Abc_Frame_t * pAbc, int * pNumEdges );
 extern void Abc_RLfLOGetEdges( Abc_Frame_t * pAbc, long * pEgdes, int nEdges, float * pEdgeFeatures);
 extern void Abc_RLfLOPrintObjNum2x(Abc_Frame_t * pAbc);
 extern void Abc_RLfLOPrintNodeIds( Abc_Frame_t * pAbc );
-void Abc_RLfLOSizeofInt(size_t * size);
+extern void Abc_RLfLOSizeofInt(size_t * size);
+extern int Abc_RLfLONtkRewrite( Abc_Frame_t * pAbc, int Id, int fUpdateLevel, int fUseZeros, int fVerbose, int fVeryVerbose, int fPlaceEnable );
+extern int Abc_RLfLONtkResubstitute( Abc_Frame_t * pAbc, int Id ,int nCutMax, int nStepsMax, int nLevelsOdc, int fUpdateLevel, int fVerbose, int fVeryVerbose );
 /*=== zzz.c ==========================================================*/
  
 
