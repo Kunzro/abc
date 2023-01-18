3278ff0e77ac932d98370d2bed54a186a68f8d62
Roman <roman@roman-pop-os.fritz.box>
2023-01-18 19:16:31 +0100

    resub, rewrite, refactor per node working

--- src/rlflo/rlflo.h
+++ src/rlflo/rlflo.h
@@ -42,6 +42,40 @@ ABC_NAMESPACE_HEADER_START
 ///                         BASIC TYPES                              ///
 ////////////////////////////////////////////////////////////////////////
 
+typedef struct Abc_ManRef_t_   Abc_ManRef_t;
+struct Abc_ManRef_t_
+{
+    // user specified parameters
+    int              nNodeSizeMax;      // the limit on the size of the supernode
+    int              nConeSizeMax;      // the limit on the size of the containing cone
+    int              fVerbose;          // the verbosity flag
+    // internal data structures
+    Vec_Ptr_t *      vVars;             // truth tables
+    Vec_Ptr_t *      vFuncs;            // functions
+    Vec_Int_t *      vMemory;           // memory
+    Vec_Str_t *      vCube;             // temporary
+    Vec_Int_t *      vForm;             // temporary
+    Vec_Ptr_t *      vVisited;          // temporary
+    Vec_Ptr_t *      vLeaves;           // temporary
+    // node statistics
+    int              nLastGain;
+    int              nNodesConsidered;
+    int              nNodesRefactored;
+    int              nNodesGained;
+    int              nNodesBeg;
+    int              nNodesEnd;
+    // runtime statistics
+    abctime          timeCut;
+    abctime          timeTru;
+    abctime          timeDcs;
+    abctime          timeSop;
+    abctime          timeFact;
+    abctime          timeEval;
+    abctime          timeRes;
+    abctime          timeNtk;
+    abctime          timeTotal;
+};
+
 ////////////////////////////////////////////////////////////////////////
 ///                      MACRO DEFINITIONS                           ///
 ////////////////////////////////////////////////////////////////////////
@@ -49,6 +83,12 @@ ABC_NAMESPACE_HEADER_START
 ////////////////////////////////////////////////////////////////////////
 ///                    FUNCTION DECLARATIONS                         ///
 ////////////////////////////////////////////////////////////////////////
+
+// declarations for Refactor
+Abc_ManRef_t * Abc_NtkManRefStart( int nNodeSizeMax, int nConeSizeMax, int fUseDcs, int fVerbose );
+Dec_Graph_t * Abc_NodeRefactor( Abc_ManRef_t * p, Abc_Obj_t * pNode, Vec_Ptr_t * vFanins, int fUpdateLevel, int fUseZeros, int fUseDcs, int fVerbose );
+void Abc_NtkManRefStop( Abc_ManRef_t * p );
+void Abc_NtkManRefPrintStats( Abc_ManRef_t * p );
 
 extern void Abc_RLfLOMapGetAreaDelay( Abc_Frame_t * pAbc, float * pArea, float * pDelay, int fAreaOnly, int useDelayTarget, double DelayTargetArg, int nTreeCRatio, int fUseWireLoads, int getDelays, double * pDelays );
 extern void Abc_RLfLOGetNodeFeatures( Abc_Frame_t * pAbc, float * x, size_t n, size_t p);
@@ -59,6 +99,7 @@ extern void Abc_RLfLOGetObjTypes( Abc_Frame_t * pAbc, int * x);
 extern void Abc_RLfLOGetNumEdges( Abc_Frame_t * pAbc, int * pNumEdges );
 extern void Abc_RLfLOGetEdges( Abc_Frame_t * pAbc, long * pEgdes, int nEdges, float * pEdgeFeatures);
 extern void Abc_RLfLOPrintNodeIds( Abc_Frame_t * pAbc );
+extern int Abc_RLfLONtkRefactor( Abc_Frame_t * pAbc, int Id, int nNodeSizeMax, int nConeSizeMax, int fUpdateLevel, int fUseZeros, int fUseDcs, int fVerbose );
 extern int Abc_RLfLONtkRewrite( Abc_Frame_t * pAbc, int Id, int fUpdateLevel, int fUseZeros, int fVerbose, int fVeryVerbose, int fPlaceEnable );
 extern int Abc_RLfLONtkResubstitute( Abc_Frame_t * pAbc, int Id ,int nCutMax, int nStepsMax, int nLevelsOdc, int fUpdateLevel, int fVerbose, int fVeryVerbose );
 /*=== zzz.c ==========================================================*/
